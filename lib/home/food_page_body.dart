91722bcf8b11f1c9a140d528fe7af26728478217
yenle254 <yenle252004@gmail.com>
2025-12-31 15:29:17 +0700

    update

--- lib/home/food_page_body.dart
+++ lib/home/food_page_body.dart
@@ -1,5 +1,6 @@
 import 'package:e_commerce/utils/colors.dart';
 import 'package:e_commerce/widgets/big_text.dart';
+import 'package:e_commerce/widgets/small_text.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -18,56 +19,76 @@ class _FoodPageBodyState extends State<FoodPageBody> {
       color: Colors.redAccent,
       height: 320,
       child: PageView.builder(
-          controller: pageController,
-          itemCount: 5,
-          itemBuilder: (context, position){
-        return _buildPageItem(position);
-      }),
+        controller: pageController,
+        itemCount: 5,
+        itemBuilder: (context, position) {
+          return _buildPageItem(position);
+        },
+      ),
     );
   }
-  Widget _buildPageItem(int index){
+
+  Widget _buildPageItem(int index) {
     return Stack(
       children: [
         Container(
           height: 220,
-          margin: EdgeInsets.only(left: 5,right: 5),
+          margin: EdgeInsets.only(left: 10, right: 10),
           decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(30),
-              color: index.isEven?Color(0xFFBBCB64):Color(0xFFFFE52A),
-              image: DecorationImage(
-                  fit: BoxFit.cover,
-                  image: AssetImage(
-                      "assets/image/food0.png"))
+            borderRadius: BorderRadius.circular(30),
+            color: index.isEven ? Color(0xFFBBCB64) : Color(0xFFFFE52A),
+            image: DecorationImage(
+              fit: BoxFit.cover,
+              image: AssetImage("assets/image/food0.png"),
+            ),
           ),
         ),
         Align(
           alignment: Alignment.bottomCenter,
           child: Container(
-            height: 140,
-            margin: EdgeInsets.only(left: 30,right: 30,bottom: 15),
+            height: 120,
+            margin: EdgeInsets.only(left: 30, right: 30, bottom: 30),
             decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(30),
-                color: Colors.white ,
+              borderRadius: BorderRadius.circular(30),
+              color: Colors.white,
             ),
             child: Container(
-              padding: EdgeInsets.only(top: 10,left: 15,right: 15),
+              padding: EdgeInsets.only(top: 10, left: 15, right: 15),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   BigText(text: "Ching chong Ding dong"),
-                  SizedBox(height: 10,),
+                  SizedBox(height: 10),
                   Row(
                     children: [
                       Wrap(
-                        children: List.generate(5,(index) { return Icon(Icons.star, color: AppColors.mainColor,size: 15,);} ),
-                      )
+                        children: List.generate(5, (index) {
+                          return Icon(
+                            Icons.star,
+                            color: AppColors.mainColor,
+                            size: 15,
+                          );
+                        }),
+                      ),
+                      SizedBox(width: 10),
+                      SmallText(text: "4.5"),
+                      SizedBox(width: 10),
+                      SmallText(text: "1287"),
+                      SizedBox(width: 10),
+                      SmallText(text: "comments"),
+                    ],
+                  ),
+                  SizedBox(height: 20), 
+                  Row(
+                    children: [
+                      
                     ],
                   )
                 ],
               ),
             ),
           ),
-        )
+        ),
       ],
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:e_commerce/home/main_food_page.dart';
+import 'package:flutter/material.dart';
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/foundation.dart'; // Để dùng kIsWeb
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    DevicePreview(
+      enabled: !kReleaseMode,
+      builder: (context) => const MyApp(), // Chạy MyApp bên trong DevicePreview
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -12,15 +20,22 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      // --- Các dòng cần thêm cho Device Preview ---
+      // useInheritedMediaQuery: true,
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
+
+      // ------------------------------------------
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+        // Đã sửa lỗi thiếu ColorScheme ở đây
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
       ),
-      home: MainFoodPage(),
+      home: const MainFoodPage(),
     );
   }
 }
 
-
+// Widget giả lập để code không báo lỗi
