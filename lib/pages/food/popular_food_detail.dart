bf2d1317b76a6f38311ece631ab0506de6aba95f
yenle254 <yenle252004@gmail.com>
2026-01-16 03:09:17 +0700

    upadate

--- lib/pages/food/popular_food_detail.dart
+++ lib/pages/food/popular_food_detail.dart
@@ -1,10 +1,14 @@
+import 'package:e_commerce/utils/colors.dart';
 import 'package:e_commerce/utils/dimensions.dart';
 import 'package:e_commerce/widgets/app_icon.dart';
+import 'package:e_commerce/widgets/big_text.dart';
+import 'package:e_commerce/widgets/icon_and_text_widget.dart';
+import 'package:e_commerce/widgets/small_text.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class PopularFoodDetail extends StatelessWidget {
-  const PopularFoodDetail({Key? key}): super(key: key);
+  const PopularFoodDetail({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -12,30 +16,99 @@ class PopularFoodDetail extends StatelessWidget {
       body: Stack(
         children: [
           Positioned(
-              left: 0,
-              right: 0,
-              child: Container(
-                width: double.maxFinite,
-                height: Dimensions.popularFoodImgSize,
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                      fit: BoxFit.cover,
-                      image: AssetImage(
-                          "assets/image/food0.png"
-                      ))
+            left: 0,
+            right: 0,
+            child: Container(
+              width: double.maxFinite,
+              height: Dimensions.popularFoodImgSize,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  fit: BoxFit.cover,
+                  image: AssetImage("assets/image/food0.png"),
                 ),
-          )),
+              ),
+            ),
+          ),
           Positioned(
-              top: Dimensions.height45,
-              left: Dimensions.width20,
-              right: Dimensions.width20,
-              child: Row(
-                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            top: Dimensions.height45,
+            left: Dimensions.width20,
+            right: Dimensions.width20,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                AppIcon(icon: Icons.arrow_back_ios),
+                AppIcon(icon: Icons.shopping_cart_checkout_outlined),
+              ],
+            ),
+          ),
+          Positioned(
+            left: 0,
+            right: 0,
+            bottom: 0,
+            top: Dimensions.popularFoodImgSize - 20,
+            child: Container(
+              padding: EdgeInsets.only(
+                left: Dimensions.width20,
+                right: Dimensions.width20,
+                top: Dimensions.height20,
+              ),
+              // tạm pause ở đây
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topRight: Radius.circular(Dimensions.radius20),
+                  topLeft: Radius.circular(Dimensions.radius20),
+                ),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  AppIcon(icon: Icons.arrow_back_ios),
-                  AppIcon(icon: Icons.shopping_cart_checkout_outlined)
+                  BigText(text: "Ching chong Ding dong"),
+                  SizedBox(height: Dimensions.height10),
+                  Row(
+                    children: [
+                      Wrap(
+                        children: List.generate(5, (index) {
+                          return Icon(
+                            Icons.star,
+                            color: AppColors.mainColor,
+                            size: 15,
+                          );
+                        }),
+                      ),
+                      SizedBox(width: 10),
+                      SmallText(text: "4.5"),
+                      SizedBox(width: 10),
+                      SmallText(text: "1287"),
+                      SizedBox(width: 10),
+                      SmallText(text: "comments"),
+                    ],
+                  ),
+                  SizedBox(height: Dimensions.height20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      IconAndTextWidget(
+                        icon: Icons.circle_sharp,
+                        text: "Tich cuc",
+                        iconColor: AppColors.iconColor1,
+                      ),
+                      IconAndTextWidget(
+                        icon: Icons.location_on,
+                        text: "1.7km",
+                        iconColor: AppColors.mainColor,
+                      ),
+                      IconAndTextWidget(
+                        icon: Icons.access_time_rounded,
+                        text: "32min",
+                        iconColor: AppColors.iconColor2,
+                      ),
+                    ],
+                  ),
                 ],
-          ))
+              ),
+            ),
+          ),
         ],
       ),
     );
--- lib/widgets/app_icon.dart
+++ lib/widgets/app_icon.dart
@@ -1,3 +1,4 @@
+import 'package:e_commerce/utils/dimensions.dart';
 import 'package:flutter/cupertino.dart';
 
 class AppIcon extends StatelessWidget {
@@ -25,7 +26,7 @@ class AppIcon extends StatelessWidget {
       child: Icon(
           icon,
           color: iconColor,
-          size: 16,
+          size: Dimensions.iconSize16,
       ),
     );
   }
